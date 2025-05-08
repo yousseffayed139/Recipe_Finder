dcd3bbd7df8cd8690c917ccedf7bd27cbdf54c05
yousseffayed139 <yousseffayed139@gmail.com>
2025-05-08 19:36:12 +0300

    Hiden key

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-OPENROUTER_API_KEY = "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"
+import os
 
 from langchain.chat_models import ChatOpenAI
 
 llm = ChatOpenAI(
     model="openrouter/qwen/qwen3-235b-a22b:free",
-    openai_api_key=OPENROUTER_API_KEY,
+    openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     base_url="https://openrouter.ai/api/v1"
 )
